15ebf25a005809c7a55012b345534139e465cf1e
shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>
2018-11-30 02:40:07 -0600

    some changes

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -1,13 +1,21 @@
 #include <nlmp_bvp.hpp>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <boost/array.hpp>
+#include <boost/numeric/odeint.hpp>
 
 using namespace Eigen;
+using namespace boost::numeric::odeint;
 
 int nlmp_bvp(
     int nEquations, int nIntervals,
     VectorXd (*dFunction) (double t, int intervalID),
     VectorXd (*BCFunction) (MatrixXd BC)
-    ){        
+    ){      
+        int k = 0, j = 1;  
+        do{
+
+        }while(true);
         return 0;
     }
 
