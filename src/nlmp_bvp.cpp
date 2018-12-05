8388315e67f81462e839f73ba9f1ab5956e28a1f
shivanandvp <shivanandvp.oss@gmail.com>
2018-12-05 01:48:09 -0600

    .

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -3,15 +3,21 @@
 #include <Eigen/Sparse>
 #include <boost/array.hpp>
 #include <boost/numeric/odeint.hpp>
+#include <boost/multiprecision/eigen.hpp>
 
 using namespace Eigen;
 using namespace boost::numeric::odeint;
+typedef Matrix<double, 1, Dynamic> ArrayXd;
+typedef Matrix<int, 1, Dynamic> ArrayXi;
 
 int nlmp_bvp(
-***REMOVED*** int nEquations, int nIntervals,
+***REMOVED*** int nEquations,
+***REMOVED*** ArrayXd samples, ArrayXi nodeID,
 ***REMOVED*** VectorXd (*dFunction) (double t, int intervalID),
 ***REMOVED*** VectorXd (*BCFunction) (MatrixXd BC)
-***REMOVED*** ){***REMOVED******REMOVED***
+***REMOVED*** ){  
+***REMOVED******REMOVED***  int nSamples = samples.size();
+***REMOVED******REMOVED***  int nIntervals = nodeID.size() - 1;
 ***REMOVED******REMOVED***  int k = 0, j = 1;  
 ***REMOVED******REMOVED***  do{
 
--- src/nlmp_bvp_test.cpp
+++ src/nlmp_bvp_test.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
-VectorXd dFunction(double t, int intervalID = 0){
-***REMOVED*** return VectorXd();
+VectorXd dFunction(double t, VectorXd x, int intervalID = 0){
+***REMOVED*** VectorXd dxdt();
+***REMOVED*** dxdt(0) = x(1);
+***REMOVED*** dxdt(1) = 0.01*(1 - (x(0))^2)*x(1) - x(0);
+***REMOVED*** return dxdt;
 }
+
 VectorXd BCFunction(MatrixXd BC){
-***REMOVED*** return VectorXd();
+***REMOVED*** VectorXd residues();
+***REMOVED*** residues(0) = 4*BC(0,0) + (BC(1,1))^2 - 8;
+***REMOVED*** residues(1) = (BC(0,0))^2 + 2*BC(1,1) - 5;
+***REMOVED*** return residues;
 }
 
 int main(int argc, char **argv)
