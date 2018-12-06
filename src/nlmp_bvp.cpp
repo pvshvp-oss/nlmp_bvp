97b8e156a8720be43f25f572b9e67d027551ad85
shivanandvp <shivanandvp.oss@gmail.com>
2018-12-06 17:37:52 -0600

    .

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -8,18 +8,21 @@ using namespace Eigen;
 using namespace boost::numeric::odeint;
 typedef VectorXd state_type;
 
+const double STEPPER_STEP = 1e-5;
+
 int nlmp_bvp(
 ***REMOVED*** int nEquations,
-***REMOVED*** VectorXd (*dFunction) (double t, VectorXd x, int intervalID),
+***REMOVED*** VectorXd startingState,
+***REMOVED*** VectorXd tNodes,
+***REMOVED*** VectorXd (*dFunction) (double t, VectorXd x),
 ***REMOVED*** VectorXd (*BCFunction) (MatrixXd BC)
 ***REMOVED*** ){  
 ***REMOVED******REMOVED***  auto dFunctionWrapper = [] (const VectorXd &x, VectorXd &dxdt, double t){
-***REMOVED******REMOVED******REMOVED******REMOVED***dxdt = dFunction(t, x, 0 /*TODO: Interval ID*/);
+***REMOVED******REMOVED******REMOVED******REMOVED***dxdt = dFunction(t, x);
 ***REMOVED******REMOVED***  }
-
 ***REMOVED******REMOVED***  int k = 0, j = 1;  
 ***REMOVED******REMOVED***  do{
-
+***REMOVED******REMOVED******REMOVED******REMOVED***integrate(dFunctionWrapper, startingState, tNodes(1), tNodes(tNodes.rows() - 1), STEPPER_STEP);
 ***REMOVED******REMOVED***  }while(true);
 ***REMOVED******REMOVED***  return 0;
 ***REMOVED*** }
--- src/nlmp_bvp_test.cpp
+++ src/nlmp_bvp_test.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-VectorXd dFunction(double t, VectorXd x, int intervalID = 0){
+VectorXd dFunction(double t, VectorXd x){
 ***REMOVED*** VectorXd dxdt(2);
 ***REMOVED*** dxdt(0) = x(1);
 ***REMOVED*** dxdt(1) = 0.01*(1.0 - pow(x(0),2.0))*x(1) - x(0);
