222eac5703efd4032ae05c9c0f05b7f36fdd1abb
shivanandvp <shivanandvp.oss@gmail.com>
2018-12-16 16:59:55 -0600

    Fixed Eigen and Odeint interoperability

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -1,39 +1,47 @@
+// ========================
+// Includes and definitions
+// ========================
 #include <nlmp_bvp.hpp>
 #include <Eigen/Eigen>
 #include <boost/array.hpp>
 #include <boost/numeric/odeint.hpp>
 #include <boost/multiprecision/eigen.hpp>
 #include <cmath>
-
 using namespace Eigen;
 using namespace boost::numeric::odeint;
-typedef std::vector<double> state_type;
+using StateType = VectorXd;
 
 const double STEPPER_STEP = 1e-5;
+// ========================
 
+// ================
+// The BVP function
+// ================
 int nlmp_bvp(
 ***REMOVED*** int nEquations,
 ***REMOVED*** VectorXd startingState,
 ***REMOVED*** VectorXd tNodes,
-***REMOVED*** VectorXd (*dFunction) (double t, VectorXd x),
-***REMOVED*** VectorXd (*BCFunction) (MatrixXd BC)
+***REMOVED*** VectorXd dFunction(double t, VectorXd x),
+***REMOVED*** VectorXd BCFunction(MatrixXd BC)
 ***REMOVED*** ){  
-***REMOVED******REMOVED***  auto dFunctionWrapper = [/*&dFunction*/] (const state_type &x, state_type &dxdt, double t){
-***REMOVED******REMOVED******REMOVED******REMOVED***// dxdt = dFunction(t, x);
-***REMOVED******REMOVED******REMOVED******REMOVED***dxdt.push_back(1.0);
-***REMOVED******REMOVED******REMOVED******REMOVED***dxdt.push_back(2.0);
+***REMOVED******REMOVED***  
+***REMOVED******REMOVED***  auto dFunctionWrapper = [dFunction] (const StateType &x, StateType &dxdt, double t){
+***REMOVED******REMOVED******REMOVED******REMOVED***dxdt = dFunction(t, x);
 ***REMOVED******REMOVED***  };
+***REMOVED******REMOVED***  
 ***REMOVED******REMOVED***  int k = 0, j = 1;  
 
-***REMOVED******REMOVED***  state_type startingStateDEMO;
-***REMOVED******REMOVED***  startingStateDEMO.push_back(1.0);
-***REMOVED******REMOVED***  startingStateDEMO.push_back(2.0);
+***REMOVED******REMOVED***  StateType startingStateDEMO;
+***REMOVED******REMOVED***  startingStateDEMO << 1.0, 2.0;
+
+***REMOVED******REMOVED***  runge_kutta_dopri5<StateType,double,StateType,double,vector_space_algebra> stepper;
 
 ***REMOVED******REMOVED***  do{
-***REMOVED******REMOVED******REMOVED******REMOVED***integrate(dFunctionWrapper, /*startingState*/ startingStateDEMO, tNodes(1), tNodes(tNodes.rows() - 1), STEPPER_STEP);
+***REMOVED******REMOVED******REMOVED******REMOVED***integrate_const(stepper, dFunctionWrapper, /*startingState*/ startingStateDEMO, tNodes(1), tNodes(tNodes.rows() - 1), STEPPER_STEP);
 ***REMOVED******REMOVED***  }while(true);
 ***REMOVED******REMOVED***  return 0;
-***REMOVED*** }
 
+***REMOVED*** }
+// ================
 
 
--- src/nlmp_bvp_test.cpp
+++ src/nlmp_bvp_test.cpp
@@ -1,22 +1,31 @@
+// ========================
+// Includes and definitions
+// ========================
 #include<iostream>
 #include<nlmp_bvp.hpp>
-
 using namespace std;
+// ========================
 
+// ===========================================================================
+// Functions that define the differential equation and its boundahy conditions
+// ===========================================================================
 VectorXd dFunction(double t, VectorXd x){
 ***REMOVED*** VectorXd dxdt(2);
 ***REMOVED*** dxdt(0) = x(1);
 ***REMOVED*** dxdt(1) = 0.01*(1.0 - pow(x(0),2.0))*x(1) - x(0);
 ***REMOVED*** return dxdt;
 }
-
 VectorXd BCFunction(MatrixXd BC){
 ***REMOVED*** VectorXd residues(2);
 ***REMOVED*** residues(0) = 4.0*BC(0,0) + pow(BC(1,1),2.0) - 8.0;
 ***REMOVED*** residues(1) = pow(BC(0,0),2.0) + 2.0*BC(1,1) - 5.0;
 ***REMOVED*** return residues;
 }
+// ============================================================================
 
+// =================
+// The main function
+// =================
 int main(int argc, char **argv)
 {
 ***REMOVED*** VectorXd startingState;
@@ -25,3 +34,4 @@ int main(int argc, char **argv)
 ***REMOVED*** cout << "Hello" << endl;
 ***REMOVED*** return 0;
 }
+// =================
