a5d90382e0f58c7bc98016a68535b7be85c8da00
shivanandvp <shivanandvp.oss@gmail.com>
2018-12-17 11:45:27 -0600

    ..

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -19,25 +19,24 @@ const double STEPPER_STEP = 1e-5;
 // ================
 int nlmp_bvp(
 ***REMOVED*** int nEquations,
-***REMOVED*** VectorXd startingState,
-***REMOVED*** VectorXd tNodes,
-***REMOVED*** VectorXd dFunction(double t, VectorXd x),
-***REMOVED*** VectorXd BCFunction(MatrixXd BC)
+***REMOVED*** StateType startingState,
+***REMOVED*** StateType tNodes,
+***REMOVED*** StateType dFunction(double t, StateType x),
+***REMOVED*** StateType BCFunction(MatrixXd BC)
 ***REMOVED*** ){  
 ***REMOVED******REMOVED***  
+***REMOVED******REMOVED***  // Capture function calls by the ODEInt library for differentials and convert it to a custom form 
 ***REMOVED******REMOVED***  auto dFunctionWrapper = [dFunction] (const StateType &x, StateType &dxdt, double t){
 ***REMOVED******REMOVED******REMOVED******REMOVED***dxdt = dFunction(t, x);
 ***REMOVED******REMOVED***  };
 ***REMOVED******REMOVED***  
 ***REMOVED******REMOVED***  int k = 0, j = 1;  
 
-***REMOVED******REMOVED***  StateType startingStateDEMO;
-***REMOVED******REMOVED***  startingStateDEMO << 1.0, 2.0;
-
 ***REMOVED******REMOVED***  runge_kutta_dopri5<StateType,double,StateType,double,vector_space_algebra> stepper;
 
 ***REMOVED******REMOVED***  do{
-***REMOVED******REMOVED******REMOVED******REMOVED***integrate_const(stepper, dFunctionWrapper, /*startingState*/ startingStateDEMO, tNodes(1), tNodes(tNodes.rows() - 1), STEPPER_STEP);
+***REMOVED******REMOVED******REMOVED******REMOVED***integrate_const(stepper, dFunctionWrapper, /*startingState*/ startingState, tNodes(1), tNodes(tNodes.rows() - 1), STEPPER_STEP);
+***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***  }while(true);
 ***REMOVED******REMOVED***  return 0;
 
--- src/nlmp_bvp_test.cpp
+++ src/nlmp_bvp_test.cpp
@@ -4,19 +4,21 @@
 #include<iostream>
 #include<nlmp_bvp.hpp>
 using namespace std;
+using namespace Eigen;
+using StateType = VectorXd;
 // ========================
 
 // ===========================================================================
 // Functions that define the differential equation and its boundahy conditions
 // ===========================================================================
-VectorXd dFunction(double t, VectorXd x){
-***REMOVED*** VectorXd dxdt(2);
+StateType dFunction(double t, StateType x){
+***REMOVED*** StateType dxdt(2);
 ***REMOVED*** dxdt(0) = x(1);
 ***REMOVED*** dxdt(1) = 0.01*(1.0 - pow(x(0),2.0))*x(1) - x(0);
 ***REMOVED*** return dxdt;
 }
-VectorXd BCFunction(MatrixXd BC){
-***REMOVED*** VectorXd residues(2);
+StateType BCFunction(MatrixXd BC){
+***REMOVED*** StateType residues(2);
 ***REMOVED*** residues(0) = 4.0*BC(0,0) + pow(BC(1,1),2.0) - 8.0;
 ***REMOVED*** residues(1) = pow(BC(0,0),2.0) + 2.0*BC(1,1) - 5.0;
 ***REMOVED*** return residues;
@@ -28,8 +30,10 @@ VectorXd BCFunction(MatrixXd BC){
 // =================
 int main(int argc, char **argv)
 {
-***REMOVED*** VectorXd startingState;
-***REMOVED*** VectorXd tNodes;
+***REMOVED*** StateType startingState(2);
+***REMOVED*** StateType tNodes(2);
+***REMOVED*** tNodes << 0.0, 1.0;
+***REMOVED*** startingState << 1.0, 2.0;
 ***REMOVED*** nlmp_bvp(2, startingState, tNodes, dFunction, BCFunction);
 ***REMOVED*** cout << "Hello" << endl;
 ***REMOVED*** return 0;
