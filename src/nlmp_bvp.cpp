e98ed13e5ac0db7707be067f2a105802a7cea261
shivanandvp <shivanandvp.oss@gmail.com>
2018-12-20 01:41:59 -0600

    swch

--- src/nlmp_bvp.cpp
+++ src/nlmp_bvp.cpp
@@ -16,8 +16,10 @@ using namespace mlinterp;
 using RowVectorXd = Matrix<double, 1, Dynamic>;
 using RowVectorXi = Matrix<int, 1, Dynamic>;
 
-const double h =0.1;
-const double epsilon = 10^(-8);
+const double h = 4e-3;
+const double epsilon = 1e-8;
+const double alpha = 15;
+const double beta = 3;
 // ========================
 
 // ================
@@ -38,24 +40,27 @@ int nlmp_bvp(
 ***REMOVED******REMOVED***  double kEpsilon = 0;
 ***REMOVED******REMOVED***  double t0;
 ***REMOVED******REMOVED***  double tm;***REMOVED*** 
+***REMOVED******REMOVED***  double kG, kP1G;
 ***REMOVED******REMOVED***  m = tNodes.cols();  
 ***REMOVED******REMOVED***  RowVectorXd IVPITSolutions, IVPPTSolutions;
 ***REMOVED******REMOVED***  RowVectorXi boundaryColumns;
 ***REMOVED******REMOVED***  VectorXd kX0;
 ***REMOVED******REMOVED***  VectorXd pX;
+***REMOVED******REMOVED***  VectorXd gkX0;
 ***REMOVED******REMOVED***  MatrixXd S;***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***  MatrixXd IVPIXSolutions, IVPPXSolutions; 
 ***REMOVED******REMOVED***  runge_kutta_dopri5<VectorXd,double,VectorXd,double,vector_space_algebra> stepper;
 
 ***REMOVED******REMOVED***  t0 = tNodes(0);
 ***REMOVED******REMOVED***  tm = tNodes(m-1);
-***REMOVED******REMOVED***  nSamples = (int)((tm - t0)/h) + 1;
+***REMOVED******REMOVED***  nSamples = floor((tm - t0)/h) + 1;
 ***REMOVED******REMOVED***  IVPIXSolutions.resize(n, nSamples);
 ***REMOVED******REMOVED***  IVPITSolutions.resize(1, nSamples);
 ***REMOVED******REMOVED***  IVPPXSolutions.resize(n, nSamples);
 ***REMOVED******REMOVED***  IVPPTSolutions.resize(1, nSamples);
 ***REMOVED******REMOVED***  boundaryColumns.resize(1, m);
 ***REMOVED******REMOVED***  kX0.resize(n,1);
+***REMOVED******REMOVED***  gkX0.resize(n,1);
 ***REMOVED******REMOVED***  pX.resize(n,1);
 ***REMOVED******REMOVED***  S.resize(n,n);***REMOVED******REMOVED***  
 ***REMOVED******REMOVED***  kX0 = x0;
@@ -88,19 +93,20 @@ int nlmp_bvp(
 ***REMOVED******REMOVED******REMOVED******REMOVED***return xSolutions(Eigen::all, BCIndices);
 ***REMOVED******REMOVED***  };
 
-***REMOVED******REMOVED***  do{***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***// Solve the initial value problem***REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***integrate_const(stepper, dFunctionWrapper, x0, t0, tm, h, odeIObserver);***REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***IVPIColumnIndex = 0;  
-
-***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"<IVPI solutions>"<<endl;
-***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"x = "<<IVPIXSolutions<<endl;
-***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"t = "<<IVPITSolutions<<endl;
-***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"</IVPI solutions>"<<endl<<endl;
-
+***REMOVED******REMOVED***  // Solve the initial value problem for the first time  
+***REMOVED******REMOVED***  integrate_const(stepper, dFunctionWrapper, kX0, t0, tm, h, odeIObserver);  
+***REMOVED******REMOVED***  IVPIColumnIndex = 0;  
+***REMOVED******REMOVED***  gkX0 = BCFunction(getBCs(IVPITSolutions, IVPIXSolutions));
+***REMOVED******REMOVED***  kG = gkX0.norm();
+***REMOVED******REMOVED***  // cout<<"<IVPI solutions>"<<endl;
+***REMOVED******REMOVED***  // cout<<"x = "<<IVPIXSolutions<<endl;
+***REMOVED******REMOVED***  // cout<<"t = "<<IVPITSolutions<<endl;
+***REMOVED******REMOVED***  // cout<<"</IVPI solutions>"<<endl<<endl;
+***REMOVED******REMOVED***  do{***REMOVED******REMOVED******REMOVED*** 
 ***REMOVED******REMOVED******REMOVED******REMOVED***for(int j = 0; j < n; j++){***REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // Determine the perturbation parameter
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** kEpsilon = max(epsilon, abs(epsilon * kX0(j,0)));
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** //kEpsilon = epsilon;
 ***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // Perturb the initial conditions***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** pX = kX0 + kEpsilon*MatrixXd::Identity(n,n).col(j);
@@ -108,7 +114,6 @@ int nlmp_bvp(
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // Solve the perturbed initial value problem***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** integrate_const(stepper, dFunctionWrapper, pX, t0, tm, h, odePObserver);
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** IVPPColumnIndex = 0;
-
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"<IVPP solutions>"<<endl;
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"x = "<<IVPPXSolutions<<endl;
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"t = "<<IVPPTSolutions<<endl;
@@ -118,17 +123,53 @@ int nlmp_bvp(
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"BC for IVPI = "<<getBCs(IVPITSolutions, IVPIXSolutions)<<endl;
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"BC for IVPP = "<<getBCs(IVPPTSolutions, IVPPXSolutions)<<endl;***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"</Calculated BCs>"<<endl<<endl;
-
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"<Calculated residues>"<<endl;
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"Residues for IVPI = "<<BCFunction(getBCs(IVPITSolutions, IVPIXSolutions))<<endl;
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"Residues for IVPP = "<<BCFunction(getBCs(IVPPTSolutions, IVPPXSolutions))<<endl;***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // cout<<"</Calculated residues>"<<endl<<endl;
 
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // Compute a column of the adjsuting matrix
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** S.col(j) = (BCFunction(getBCs(IVPPTSolutions, IVPPXSolutions))- BCFunction(getBCs(IVPITSolutions, IVPIXSolutions)))/kEpsilon;***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** // Compute a column of the adjusting matrix***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** S.col(j) = (BCFunction(getBCs(IVPPTSolutions, IVPPXSolutions))- gkX0)/kEpsilon;***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
 ***REMOVED******REMOVED******REMOVED******REMOVED***}
-***REMOVED******REMOVED******REMOVED******REMOVED***cout<<"S = "<<S<<endl;
-***REMOVED******REMOVED******REMOVED******REMOVED***break;
+
+***REMOVED******REMOVED******REMOVED******REMOVED***// Solve the linarized adjusting equation
+***REMOVED******REMOVED******REMOVED******REMOVED***kX0 = S.colPivHouseholderQr().solve(-gkX0) + kX0;
+
+***REMOVED******REMOVED******REMOVED******REMOVED***// Solve the initial value problem***REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***integrate_const(stepper, dFunctionWrapper, kX0, t0, tm, h, odeIObserver);***REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***IVPIColumnIndex = 0; 
+***REMOVED******REMOVED******REMOVED******REMOVED***gkX0 = BCFunction(getBCs(IVPITSolutions, IVPIXSolutions));
+***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"<IVPI solutions>"<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"x = "<<IVPIXSolutions<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"t = "<<IVPITSolutions<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED***// cout<<"</IVPI solutions>"<<endl<<endl;
+
+***REMOVED******REMOVED******REMOVED******REMOVED***kP1G = gkX0.norm();
+
+***REMOVED******REMOVED******REMOVED******REMOVED***if(kP1G <= pow(10, -alpha)){
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"Stopping criterion reached..."<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"<IVPI solutions>"<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"x(t0) = "<<IVPIXSolutions.col(0)<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"x(tm) = "<<IVPIXSolutions.col(m-1)<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"t = "<<tNodes<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"</IVPI solutions>"<<endl<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** break;
+***REMOVED******REMOVED******REMOVED******REMOVED***} else if(pow(10, -alpha) < kP1G && kP1G <= pow(10, -beta) && kP1G/kG < 1){***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** 
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** ++k;***REMOVED******REMOVED******REMOVED***  
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"Stopping conditon = "<<kP1G - pow(10, -alpha)<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"Proceeding for iteration "<<k<<"..."<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED***} else{
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"Singular problem..."<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"k = "<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"pow(10, -alpha) = "<<pow(10, -alpha)<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"kP1G = "<<kP1G<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"pow(10, -beta) = "<<pow(10, -beta)<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"kG = "<<kG<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cout<<"kP1G/kG = "<<kP1G/kG<<endl;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** break;
+***REMOVED******REMOVED******REMOVED******REMOVED***}
+
+***REMOVED******REMOVED******REMOVED******REMOVED***kG = kP1G;
 ***REMOVED******REMOVED***  }while(true);***REMOVED*** 
 ***REMOVED******REMOVED***  return 0;
 ***REMOVED*** }
--- src/nlmp_bvp_test.cpp
+++ src/nlmp_bvp_test.cpp
@@ -12,15 +12,24 @@ using RowVectorXd = Matrix<double, 1, Dynamic>;
 // Functions that define the differential equation and its boundary conditions
 // ===========================================================================
 VectorXd dFunction(double t, VectorXd x){
-***REMOVED*** VectorXd dxdt(2);
+***REMOVED*** VectorXd dxdt(6);
+***REMOVED*** double r = sqrt(pow(x(0),2) + pow(x(2),2) + pow(x(4),2));
 ***REMOVED*** dxdt(0) = x(1);
-***REMOVED*** dxdt(1) = 0.01*(1.0 - pow(x(0),2.0))*x(1) - x(0);
+***REMOVED*** dxdt(1) = -x(0) / pow(r,3);
+***REMOVED*** dxdt(2) = x(3);
+***REMOVED*** dxdt(3) = -x(2) / pow(r,3);
+***REMOVED*** dxdt(4) = x(5);
+***REMOVED*** dxdt(5) = -x(4) / pow(r,3);
 ***REMOVED*** return dxdt;
 }
 VectorXd BCFunction(MatrixXd BC){
-***REMOVED*** VectorXd residues(2);
-***REMOVED*** residues(0) = 4.0*BC(0,0) + pow(BC(1,1),2.0) - 8.0;
-***REMOVED*** residues(1) = pow(BC(0,0),2.0) + 2.0*BC(1,1) - 5.0;
+***REMOVED*** VectorXd residues(6);
+***REMOVED*** residues(0) = BC(0,0) - 1.076;
+***REMOVED*** residues(1) = BC(1,0) + pow(BC(1,0),2) + BC(1,1) + BC(1,2) + BC(1,3) + 2.053292953504164;
+***REMOVED*** residues(2) = BC(2,0) + BC(3,0) - 0.472283099142472;
+***REMOVED*** residues(3) = pow(BC(3,1),2) + BC(4,2) - 1.040204804411078;
+***REMOVED*** residues(4) = BC(2,3) + BC(4,3) - 1.57661; 
+***REMOVED*** residues(5) = BC(5,3) + 0.03407297218269353;
 ***REMOVED*** return residues;
 }
 // ============================================================================
@@ -30,11 +39,11 @@ VectorXd BCFunction(MatrixXd BC){
 // =================
 int main(int argc, char **argv)
 {
-***REMOVED*** VectorXd startingState(2);
-***REMOVED*** RowVectorXd tNodes(2);
-***REMOVED*** tNodes << 0.0, 1.0;
-***REMOVED*** startingState << 0.5, 0.5;
-***REMOVED*** nlmp_bvp(2, startingState, tNodes, dFunction, BCFunction);
+***REMOVED*** VectorXd startingState(6);
+***REMOVED*** RowVectorXd tNodes(4);
+***REMOVED*** tNodes << 0.0, 0.8, 1.4, 2;
+***REMOVED*** startingState << 1.076, 0.538, 0.0, 0.288, 0.0, 0.49883;
+***REMOVED*** nlmp_bvp(6, startingState, tNodes, dFunction, BCFunction);
 ***REMOVED*** return 0;
 }
 // =================
