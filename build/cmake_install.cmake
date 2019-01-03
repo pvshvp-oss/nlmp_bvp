e15b9c6789a73859f9787e32c9beb516895b98ee
shivanand.pattanshetti <shivanand.pattanshetti@gmail.com>
2019-01-02 21:11:25 -0600

    .

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/shivanandvp/Development/nlmp_bvp
+# Install script for directory: /home/shivanandvp/Development/nlmpbvp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/shivanandvp/Development/nlmp_bvp/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/shivanandvp/Development/nlmpbvp/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
